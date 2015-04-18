626b484bc108c0a076fac41a75ac4428421d1ea5
Noah Mostacero <noahmostacero@gmail.com>
2015-04-17 18:37:45 -0700

    Finished milestone 9

--- index.php
+++ index.php
@@ -24,4 +24,23 @@ and open the template in the editor.
         </form>
         </div>
     </body>
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script>
+//        calling the add task function
+        add_task();
+        
+        function add_task(){
+            $('.add-new-task').submit(function()) {
+                var new_task = $('.add-new-task input[name=new-task]').val();
+                
+                if(new_task != '') {
+                    $.post('includes/add-task.php', { task: new_task}, function(data){
+                        $(('add-new-task input[name=new-task]').val();
+                                $(data).appendTo('task-list ul').hide().fadeIn();
+                    });
+                }
+                return false;
+            });
+        }
+    </script>
 </html>
